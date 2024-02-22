8f1f4074e707d26dc3d331641ba3a9f4dee51da3
robertotics4 <robertooliveiragp@gmail.com>
2024-02-21 22:28:55 -0300

    feat: get user id from request on match creation

--- src/main/interfaces/rest/controllers/matches/CreateMatchController.ts
+++ src/main/interfaces/rest/controllers/matches/CreateMatchController.ts
@@ -4,9 +4,13 @@ import { Request, Response } from 'express';
 
 export class CreateMatchController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { user } = request;
     const createMatchUseCase = container.resolve(CreateMatchUseCase);
 
-    const result = await createMatchUseCase.execute(request.body);
+    const result = await createMatchUseCase.execute({
+      ...request.body,
+      userId: user.id,
+    });
 
     return response.status(201).json(result);
   }
