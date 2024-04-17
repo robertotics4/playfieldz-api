9eae1e1e77ae9b99af4445e6d201c2e066f4e80b
robertotics4 <robertooliveiragp@gmail.com>
2024-04-17 16:51:16 -0300

    feat: autopopulate user in group find

--- src/infra/database/mongoose/schemas/GroupSchema.ts
+++ src/infra/database/mongoose/schemas/GroupSchema.ts
@@ -1,3 +1,4 @@
+import mongooseAutopopulate from 'mongoose-autopopulate';
 import { Group } from '@/domain';
 import { Schema, model } from 'mongoose';
 
@@ -9,13 +10,20 @@ const PlayerSubscriptionSchema = new Schema({
 const GroupSchema = new Schema(
   {
     name: { type: String, required: true },
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      autopopulate: true,
+    },
     playerSubscriptions: [PlayerSubscriptionSchema],
     description: String,
     imageUrl: String,
   },
   { timestamps: true },
 );
+
+GroupSchema.plugin(mongooseAutopopulate);
 
 GroupSchema.loadClass(Group);
 const GroupModel = model('Group', GroupSchema);
